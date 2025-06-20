288dba4902dfccf70eff54c040c44309a1b8a6ac
Tri Dung Nguyen <tridung.190705@gmail.com>
2025-06-20 04:49:40 +0000

    [automatic commit] 2025-06-20 04:49:40 UTC 119.15.78.5

--- part1/app.js
+++ part1/app.js
@@ -18,6 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+const pool = mysql.cr
 // 1. /api/dogs
 
 // 2. /api/walkrequests/open
