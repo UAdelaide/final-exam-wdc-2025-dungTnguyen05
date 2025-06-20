e42a2546cbc38a326a50310364a2674b5fc20b43
Tri Dung Nguyen <tridung.190705@gmail.com>
2025-06-20 10:36:20 +0000

    [automatic commit] 2025-06-20 10:36:20 UTC 119.15.78.5

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -59,7 +59,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
-// POST logout
+// POST logout (Q14)
 router.post('/logout', (req, res) => {
   req.session.destroy((err) => {
     if (err) {
