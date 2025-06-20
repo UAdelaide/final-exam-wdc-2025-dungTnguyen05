0c5e1d57284de25476066e29cfa79ae395960b8a
Tri Dung Nguyen <tridung.190705@gmail.com>
2025-06-20 08:09:22 +0000

    [automatic commit] 2025-06-20 08:09:22 UTC 119.15.78.5

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -50,7 +50,7 @@ router.post('/login', async (req, res) => {
     }
 
     // Store user in session
-    req
+    req.session.user = rows[0];
 
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
