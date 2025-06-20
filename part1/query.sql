fad7a5ebaabace7ae472c58cd8af946cd4489d23
Tri Dung Nguyen <tridung.190705@gmail.com>
2025-06-20 04:03:26 +0000

    [automatic commit] 2025-06-20 04:03:26 UTC 119.15.78.5

--- part1/query.sql
+++ part1/query.sql
@@ -2,12 +2,12 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-('tridung', 'tridung.190705@gmail.com', 'hashed', 'walker'),
-('namphuong', 'tranphuonggg04@gmail.com', 'hashed', 'owner');
+('tridung', 'tridung.190705@gmail.com', 'hashed', 'owner'),
+('namphuong', 'tranphuonggg04@gmail.com', 'hashed', 'walker');
 
 INSERT INTO Dogs (name, size, owner_id) VALUES
 ('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
 ('Bella', 'small', (SELECT user_id FROM Users WHERE username = 'carol123')),
 ('dotrung', 'large', (SELECT user_id FROM Users WHERE username = 'tridung')),
 ('dog1', 'medium', (SELECT user_id FROM Users WHERE username = 'namphuong')),
-('dog2', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
+('dog2', 'medium', (SELECT user_id FROM Users WHERE username = '')),
