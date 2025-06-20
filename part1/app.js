4764f44360b62478f24cf9e3773542c312981f78
Tri Dung Nguyen <tridung.190705@gmail.com>
2025-06-20 05:16:28 +0000

    [automatic commit] 2025-06-20 05:16:28 UTC 119.15.78.5

--- part1/app.js
+++ part1/app.js
@@ -50,7 +50,8 @@ app.get('/api/walkrequests/open', async (req, res) => {
             SELECT wr.request_id, d.name AS dog_name, wr.requested_time,
             wr.duration_minutes, wr.location, u.username AS owner_username
             FROM WalkRequests wr
-            JOIN 
+            JOIN Dogs d ON wr.dog_id = d.dog_id
+            JOIN Users u ON d
         `);
     }
 
