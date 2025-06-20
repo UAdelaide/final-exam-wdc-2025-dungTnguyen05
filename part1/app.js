f09cb18fa95716d5cba4a169e41c23c77419106d
Tri Dung Nguyen <tridung.190705@gmail.com>
2025-06-20 05:06:51 +0000

    [automatic commit] 2025-06-20 05:06:51 UTC 119.15.78.5

--- part1/app.js
+++ part1/app.js
@@ -30,6 +30,8 @@ app.get('/api/dogs', async (req, res) => {
     try {
         const [rows] = await pool.query(`
             SELECT d.name AS dog_name, d.size, u.username AS owner_username
+            FROM Dogs d
+            JOIN Users u ON d.owner_id = u.user_id
         `);
 
         res.json(rows);
