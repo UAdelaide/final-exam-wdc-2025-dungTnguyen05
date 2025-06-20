7129a9b637fedbabafb0a21b5ed16e58ef2e6ce3
Tri Dung Nguyen <tridung.190705@gmail.com>
2025-06-20 03:56:14 +0000

    [automatic commit] 2025-06-20 03:56:14 UTC 119.15.78.5

--- part1/query.sql
+++ part1/query.sql
@@ -2,3 +2,4 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+('tridung', 'tridung.190705@gmail.com', 'hashed', 'walker'),
