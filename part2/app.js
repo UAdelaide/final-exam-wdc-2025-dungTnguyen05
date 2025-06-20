cfa9f0f7829200ebd51c156433e6163eb5cb5908
Tri Dung Nguyen <tridung.190705@gmail.com>
2025-06-20 09:59:41 +0000

    [automatic commit] 2025-06-20 09:59:41 UTC 119.15.78.5

--- part2/app.js
+++ part2/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const session = require('express-session'); // session
+const session = require('express-session'); // For session
 require('dotenv').config();
 
 const app = express();
