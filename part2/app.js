6c6fb6878d1162d7fee27281119c06d05979c4f9
Tri Dung Nguyen <tridung.190705@gmail.com>
2025-06-20 08:01:49 +0000

    [automatic commit] 2025-06-20 08:01:49 UTC 119.15.78.5

--- part2/app.js
+++ part2/app.js
@@ -9,6 +9,11 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
+// Session middleware
+app.use(session({
+    cookie: 
+}));
+
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
