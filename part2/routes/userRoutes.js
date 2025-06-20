cc3c34b6a13b3624b7027e5b15ed6d003878f792
Tri Dung Nguyen <tridung.190705@gmail.com>
2025-06-20 08:09:01 +0000

    [automatic commit] 2025-06-20 08:09:01 UTC 119.15.78.5

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,6 +49,9 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
+    // Store user in session
+    req
+
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
