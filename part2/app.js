5b11852620d421b6a8c33de615dbca2efcadf5d8
Tri Dung Nguyen <tridung.190705@gmail.com>
2025-06-20 08:04:34 +0000

    [automatic commit] 2025-06-20 08:04:34 UTC 119.15.78.5

--- part2/app.js
+++ part2/app.js
@@ -13,6 +13,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use(session({
     secret: '',
     resave: false,
+    saveUninitialized: false,
     cookie: { secure: false, maxAge: 60000 }
 }));
 
