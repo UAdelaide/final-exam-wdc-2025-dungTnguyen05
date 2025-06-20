4d9ef311df13a7442a308cddc1d4b385b591c46d
Tri Dung Nguyen <tridung.190705@gmail.com>
2025-06-20 13:40:48 +0000

    [automatic commit] 2025-06-20 13:40:48 UTC 119.15.78.5

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -88,4 +88,20 @@ router.get('/dogs', async (req, res) => {
   }
 });
 
+// GET all dogs (for homepage display)
+router.get('/all-dogs', async (req, res) => {
+  try {
+    const [rows] = await db.query(`
+      SELECT d.name AS dog_name, d.size, u.username AS owner_username
+      FROM Dogs d
+      JOIN Users u ON d.owner_id = u.user_id
+    `);
+
+    res.json(rows);
+  } catch (error) {
+    console.error('SQL Error:', error);
+    res.status(500).json({ error: 'Failed to fetch dogs' });
+  }
+});
+
 module.exports = router;
